0f95a06c2698e2a5a2bd4259a2789431aaecc6c8
backskin <vbackskin@gmail.com>
2019-10-01 04:37:13 +0400

    6-й cummit

--- my-tests.py
+++ my-tests.py
@@ -18,6 +18,7 @@ from stegos import insert_dwm, extract_dwm, insert_dwm_wkey, extract_dwm_wkey
 
 
 # TEST 2: inception with key
+#
 # image, dwm = imread('cont.png'), imread('dwm2.bmp')
 # key = 'hello_world'
 # wrong_key = 'bye_bye'
@@ -32,33 +33,61 @@ from stegos import insert_dwm, extract_dwm, insert_dwm_wkey, extract_dwm_wkey
 # imshow(wr_dwm_layer)
 # plt.show()
 
+# TEST 3: check simple inception and extraction after noise
+#
+# image, dwm = imread('cont.png'), imread('dwm2.bmp')
+# im_with_dwm = insert_dwm(image, dwm)
+# imsave('cont-with-dwm.png', im_with_dwm)
+# imshow(im_with_dwm)
+# plt.show()
+# dwm_layer = extract_dwm(im_with_dwm)
+# imshow(dwm_layer)
+# plt.show()
+# im_with_dwm = noising_by_yuv(im_with_dwm.copy(), 0.5)
+# imsave('cont-with-dwm-noised.png', im_with_dwm)
+# again_img = imread('cont-with-dwm-noised.png')
+# imshow(again_img)
+# plt.show()
+# dwm_layer = extract_dwm(again_img)
+# imshow(dwm_layer)
+# plt.show()
 
-image = imread('tiger-color.png')
+# TEST 4: check simple inc and ext after sharpen
+#
+# image, dwm = imread('tiger-color.png'), imread('dwm2.bmp')
+#
+# sharp_mask = 1 / 8 * np.array([[-1, -1, -1],
+#                                [-1, 16, -1],
+#                                [-1, -1, -1]])[:, :]
+#
+# im_with_dwm = insert_dwm(image, dwm)
+# dwm_layer = extract_dwm(im_with_dwm)
+# imshow(dwm_layer)
+# plt.show()
+# im_with_dwm = convolution_rgb(im_with_dwm.copy(), sharp_mask)
+# imsave('cont-with-dwm-sharp.png', im_with_dwm)
+# again_img = imread('cont-with-dwm-sharp.png')
+# imshow(again_img)
+# plt.show()
+# dwm_layer = extract_dwm(again_img)
+# imsave('dwm-layer-sharpen.png', dwm_layer)
+# imshow(dwm_layer)
+# plt.show()
 
-
-img = noising_by_yuv(image.copy(), 32)
-imsave('tiger-color-noise_yuv.png', img)
-imshow(img)
+# TEST 5: check simple inc and ext after median filter
+image, dwm = imread('tiger-color.png'), imread('dwm2.bmp')
+im_with_dwm = insert_dwm(image, dwm)
+dwm_layer = extract_dwm(im_with_dwm)
+imshow(dwm_layer)
+plt.show()
+im_with_dwm = rng_filter_rgb(median, im_with_dwm.copy(), [[1] * 3] * 3)
+imsave('cont-with-dwm-median.png', im_with_dwm)
+again_img = imread('cont-with-dwm-median.png')
+imshow(again_img)
+plt.show()
+dwm_layer = extract_dwm(again_img)
+imsave('dwm-layer-median.png', dwm_layer)
+imshow(dwm_layer)
 plt.show()
 
-img = noising_rgb(image.copy(), 32)
-imsave('tiger-color-noise_rgb.png', img)
-imshow(img)
-plt.show()
-
-
-sharp_mask = np.array([[-1, -1, -1],
-                       [-1, 16, -1],
-                       [-1, -1, -1]])
-
-sharp_mask = sharp_mask[:, :] * 1/8
-
-img = convolution_rgb(image.copy(), sharp_mask)
-imsave('tiger-color-sharp.png', img)
-imshow(img)
-plt.show()
-
-img = rng_filter_rgb(median, image.copy(), [[1] * 3] * 3)
-imsave('tiger-color-median.png', img)
-imshow(img)
-plt.show()
+# TEST 6: check simple inc and ext after dilatation and erosion filters
